2f594296bea9be4086985aba9b4b469f843992d2
Martin <hello@northrook.com>
2025-01-13 07:01:15 +0000

    mend

--- src/Highlight.php
+++ src/Highlight.php
@@ -8,6 +8,7 @@ use Tempest\Highlight\{Highlighter, Language, Languages\Css\CssLanguage, Languag
 use Stringable;
 use Tempest\Highlight\Languages\Html\HtmlLanguage;
 use Tempest\Highlight\Languages\Text\TextLanguage;
+use ValueError;
 
 final class Highlight implements Stringable
 {
@@ -15,11 +16,16 @@ final class Highlight implements Stringable
 
     public readonly Language $language;
 
+    public readonly string $code;
+
+    private readonly string $source;
+
     public function __construct(
-        protected string     $code,
-        null|string|Language $language = AUTO,
-        protected ?int       $gutter = null,
+        null|string|Stringable $code,
+        null|string|Language   $language = AUTO,
+        protected ?int         $gutter = null,
     ) {
+        $this->source      = (string) $code;
         $this->highlighter = new Highlighter();
 
         if ( AUTO === $language ) {
@@ -32,11 +38,14 @@ final class Highlight implements Stringable
             $this->highlighter->withGutter( $gutter );
             // return $highlighter->withGutter( $gutter )->parse( $code, $language );
         }
+
+        $this->code     = $this->highlighter->parse( $this->source, $language );
+        $this->language = $this->highlighter->getCurrentLanguage() ?? throw new ValueError();
     }
 
     public function __toString() : string
     {
-        return $this->highlighter->parse( $this->code, $this->language );
+        return $this->code;
     }
 
     public static function code(
@@ -64,20 +73,20 @@ final class Highlight implements Stringable
 
     private function isHtml() : bool
     {
-        return (bool) \preg_match( '#^\h*<[a-z-:]*.+>\s*$#m', $this->code );
+        return (bool) \preg_match( '#^\h*<[a-z-:]*.+>\s*$#m', $this->source );
     }
 
     private function isCss() : bool
     {
-        if ( (bool) \preg_match( '/^[.:#]/m', $this->code ) ) {
+        if ( \preg_match( '/^[.:#]/m', $this->source ) ) {
             return true;
         }
 
-        return (bool) \preg_match( '/^\h*[a-zA-Z].+:.+;/m', $this->code );
+        return (bool) \preg_match( '/^\h*[a-zA-Z].+:.+;/m', $this->source );
     }
 
     private function isPhp() : bool
     {
-        return \str_starts_with( $this->code, '<?php' );
+        return \str_starts_with( $this->source, '<?php' );
     }
 }
